ed4a1e2c674ed52eacf37a34196fe389305ddc26
Grant Nestor <grantnestor@gmail.com>
2016-12-06 13:24:34 -0800

    Rewrite nbextension in ES6

--- {{cookiecutter.extension_name}}/nbextension/src/renderer.js
+++ {{cookiecutter.extension_name}}/nbextension/src/renderer.js
@@ -1,52 +1,45 @@
-require('./index.css');
+import './index.css';
 
-var MIME_TYPE = '{{cookiecutter.mime_type}}';
+const MIME_TYPE = '{{cookiecutter.mime_type}}';
 
 //
 // Render data to the output area
 // 
 function render(data, node) {
-    var text = document.createTextNode(JSON.stringify(data));
-    node.appendChild(text);
+  const text = document.createTextNode(JSON.stringify(data));
+  node.appendChild(text);
 }
 
 //
 // Register the mime type and append_mime_type function with the notebook's OutputArea
 // 
-function register_renderer($) {
-    // Get an instance of the OutputArea object from the first CodeCellebook_
-    var OutputArea = $('#notebook-container').find('.code_cell').eq(0).data('cell').output_area;
-    // A function to render output of 'application/vnd.plotly.v1+json' mime type
-    var append_json = function (json, md, element) {
-        var type = MIME_TYPE;
-        var toinsert = this.create_output_subarea(md, 'output_{{cookiecutter.mime_short_name}} rendered_html', type);
-        this.keyboard_manager.register_events(toinsert);
-        render(json, toinsert[0]);
-        element.append(toinsert);
-        return toinsert;
-    };
-    // Register the mime type and append_mime_type function with the notebook's OutputArea
-    OutputArea.register_mime_type(MIME_TYPE, append_json, true);
+export function register_renderer($) {
+  // Get an instance of the OutputArea object from the first CodeCellebook_
+  const OutputArea = $('#notebook-container').find('.code_cell').eq(0).data('cell').output_area;
+  // A function to render output of 'application/vnd.plotly.v1+json' mime type
+  const append_json = function(json, md, element) {
+    const type = MIME_TYPE;
+    const toinsert = this.create_output_subarea(md, 'output_{{cookiecutter.mime_short_name}} rendered_html', type);
+    this.keyboard_manager.register_events(toinsert);
+    render(json, toinsert[0]);
+    element.append(toinsert);
+    return toinsert;
+  };
+  // Register the mime type and append_mime_type function with the notebook's OutputArea
+  OutputArea.register_mime_type(MIME_TYPE, append_json, true);
 }
 
 //
 // Re-render cells with output data of 'application/vnd.plotly.v1+json' mime type
 // 
-function render_cells($) {
-    // Get all cells in notebook
-    $('#notebook-container').find('.cell').toArray().forEach(function(item, index) {
-        var CodeCell = $(item).data('cell');
-        // If a cell has output data of 'application/vnd.plotly.v1+json' mime type
-        if (CodeCell.output_area && CodeCell.output_area.outputs.find(function(output) {
-            return output.data[MIME_TYPE];
-        })) {
-            // Re-render the cell by executing it
-            CodeCell.notebook.render_cell_output(CodeCell);
-        }
-    });
+export function render_cells($) {
+  // Get all cells in notebook
+  $('#notebook-container').find('.cell').toArray().forEach(item => {
+    const CodeCell = $(item).data('cell');
+    // If a cell has output data of 'application/vnd.plotly.v1+json' mime type
+    if (CodeCell.output_area && CodeCell.output_area.outputs.find(output => output.data[MIME_TYPE])) {
+      // Re-render the cell by executing it
+      CodeCell.notebook.render_cell_output(CodeCell);
+    }
+  });
 }
-
-module.exports = {
-    register_renderer: register_renderer,
-    render_cells: render_cells
-};
