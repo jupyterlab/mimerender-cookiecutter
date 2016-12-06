f35dfa4e3fb3d38c1e1021b32cab5ab90e794029
Grant Nestor <grantnestor@gmail.com>
2016-12-06 14:09:42 -0800

    Demonstrate how to use `OutputArea.mime_types` to calculate the renderer's `display_order` index

--- {{cookiecutter.extension_name}}/nbextension/src/renderer.js
+++ {{cookiecutter.extension_name}}/nbextension/src/renderer.js
@@ -17,7 +17,7 @@ export function register_renderer($) {
   // Get an instance of the OutputArea object from the first CodeCellebook_
   const OutputArea = $('#notebook-container').find('.code_cell').eq(0).data('cell').output_area;
   // A function to render output of 'application/vnd.plotly.v1+json' mime type
-  const append_json = function(json, md, element) {
+  const append_mime = function(json, md, element) {
     const type = MIME_TYPE;
     const toinsert = this.create_output_subarea(md, 'output_{{cookiecutter.mime_short_name}} rendered_html', type);
     this.keyboard_manager.register_events(toinsert);
@@ -25,8 +25,18 @@ export function register_renderer($) {
     element.append(toinsert);
     return toinsert;
   };
+  // Calculate the index of this renderer in `OutputArea.display_order` or pass an integer
+  const mime_types = OutputArea.mime_types();
+  const json_types = mime_types.filter(mimetype => mimetype.includes('+json'));
+  // Insert this renderer after any renderers with mime type that matches "application/*+json"
+  const index = mime_types.lastIndexOf(json_types.pop() + 1);
   // Register the mime type and append_mime_type function with the notebook's OutputArea
-  OutputArea.register_mime_type(MIME_TYPE, append_json, true);
+  OutputArea.register_mime_type(MIME_TYPE, append_mime, {
+    // Is output safe?
+    safe: false,
+    // Index of renderer in `OutputArea.display_order`
+    index: index
+  });
 }
 
 //
