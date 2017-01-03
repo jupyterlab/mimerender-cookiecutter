34063e0dfcf5b0aa8a8fb47d9e8e65edf53ad22c
Grant Nestor <grantnestor@gmail.com>
2017-01-02 23:50:31 -0800

    Update nbextension webpack loader config to match labextension

--- {{cookiecutter.extension_name}}/nbextension/webpack.config.js
+++ {{cookiecutter.extension_name}}/nbextension/webpack.config.js
@@ -3,21 +3,40 @@ var version = require('./package.json').version;
 // Custom webpack loaders are generally the same for all webpack bundles, hence
 // stored in a separate local variable.
 var loaders = [
-    { 
-        test: /\.json$/, 
-        loader: 'json-loader' 
-    },
-    { 
-        test: /\.js$/, 
-        exclude: /node_modules/, 
+    {
+        test: /\.js$/,
+        exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
             presets: ['latest', 'stage-0', 'react']
         }
-    },
-    {
+    }, {
+        test: /\.json$/,
+        loader: 'json-loader'
+    }, {
         test: /\.css$/,
         loader: 'style-loader!css-loader'
+    }, {
+        test: /\.html$/,
+        loader: 'file-loader'
+    }, {
+        test: /\.(jpg|png|gif)$/,
+        loader: 'file-loader'
+    }, {
+        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+    }, {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+    }, {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+    }, {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader'
+    }, {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
     }
 ];
 
