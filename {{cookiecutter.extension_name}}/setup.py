1f7bd3887f2cbfe40652972fd98e97e9f8fc463b
Grant Nestor <grantnestor@gmail.com>
2017-03-23 19:07:13 -0700

    Use jupyter-packaging

--- {{cookiecutter.extension_name}}/setup.py
+++ {{cookiecutter.extension_name}}/setup.py
@@ -1,24 +1,8 @@
-import os
-import sys
-from distutils.core import setup
+from setuptools import setup
+from setupbase import create_cmdclass, install_npm
 
-
-class NodeModulesMissing(Exception):
-    "raised when node_modules directory is not found"
-    pass
-
-
-if 'develop' in sys.argv or any(a.startswith('bdist') for a in sys.argv):
-    import setuptools
-
-# Ensure that js has been built. This does not guaruntee that the packages
-# are update to date. In the future we might do a more expensive check
-# involving file hashes, but only on sdist and bdist builds.
-if not os.path.exists('labextension/node_modules') and not os.path.exists('nbextension/node_modules'):
-    raise NodeModulesMissing("Before Python package can be installed or built, "
-                             "JavaScript components must be built using npm. "
-                             "Run the following and then retry: "
-                             "\nnpm install")
+cmdclass = create_cmdclass(['js'])
+cmdclass['js'] = install_npm()
 
 setup_args = dict(
     name                 = '{{cookiecutter.extension_name}}',
@@ -26,10 +10,26 @@ setup_args = dict(
     packages             = ['{{cookiecutter.extension_name}}'],
     author               = '{{cookiecutter.author_name}}',
     author_email         = '{{cookiecutter.author_email}}',
-    keywords             = ['jupyter', 'jupyterlab', 'labextension', 'notebook', 'nbextension'],
-    include_package_data = True,
+    url                  = 'http://jupyter.org',
+    license              = 'BSD',
+    platforms            = "Linux, Mac OS X, Windows",
+    keywords             = ['ipython', 'jupyter'],
+    classifiers          = [
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    cmdclass         = cmdclass,
     install_requires = [
         'jupyterlab>=0.18.0',
+        'notebook>=4.3.0',
         'ipython>=1.0.0'
     ]
 )
